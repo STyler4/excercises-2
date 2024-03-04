f17b37ad9a8a670a54920aa82656257522bc8d00
STyler4 <98915469+STyler4@users.noreply.github.com>
2024-03-05 12:14:52 +1300

    Update studentClass1.py

--- studentClass1.py
+++ studentClass1.py
@@ -7,6 +7,43 @@ class Student:
 
     # method to return student grade
     def get_grade(self):
-        return self.grade
+        return f"{self.name} got {self.grade}"
 
-    # Instantiate student objects
+    # method to get average grade
+    def get_average_grade(self):
+        pass
+
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []  # Empty list
+
+    def add_student(self, student):
+        # Test that there is room in the course
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return False
+        return True
+
+
+# Instantiate 3 student objects
+s1 = Student(name="Tim", age=19, grade=95)
+s2 = Student(name="Bill", age=19, grade=75)
+s3 = Student(name="Jill", age=19, grade=65)
+
+# print(Student.get_grade(s1))
+# print(Student.get_grade(s2))
+# print(Student.get_grade(s3))
+
+# instantiate course subject
+course1 = Course(name="Computer Science", max_students=2)
+
+course1.add_student(s1)
+course1.add_student(s2)
+# course1.add_student(s3)
+
+# confirm entry of students
+for student in course1.students:
+    print(student.name)
